76a8611a5d5b4f2b5ef0c2e1e3a2254fb4ddf4ae
pojol <pojol@outlook.com>
2020-10-12 19:22:59 +0800

    # feat - Add increase mode bot

--- bot/main.go
+++ bot/main.go
@@ -27,6 +27,11 @@ var (
 
 	// robot number
 	num int
+
+	// increase
+	increase bool
+
+	lifetime int
 )
 
 func initFlag() {
@@ -35,6 +40,8 @@ func initFlag() {
 	flag.StringVar(&target, "target", "http://localhost:14001", "set target server address")
 	flag.StringVar(&strategy, "strategy", "login", "set strategy `login` & `loginout` ")
 	flag.IntVar(&num, "num", 1, "robot number")
+	flag.BoolVar(&increase, "increase", false, "incremental robot in every second")
+	flag.IntVar(&lifetime, "lifetime", 60, "life time by second")
 }
 
 /*
@@ -48,7 +55,121 @@ gate1+->+
         +---->base2
 */
 
-var tokenm map[string]int
+func report(info map[string][]botreport.Info) {
+	for k := range info {
+		t, err := botreport.GetAverageTime(info[k])
+		if err != nil {
+			continue
+		}
+
+		rate := botreport.GetSuccRate(info[k])
+
+		fmt.Printf("%-30s Req count %-5d Average time %-5s Succ rate %-10s \n", k, len(info[k]), strconv.Itoa(t)+"ms", rate)
+	}
+}
+
+func reportByMap(m map[string]*gobot.Bot) {
+	minfo := make(map[string][]botreport.Info)
+	for _, v := range m {
+		info := v.GetReportInfo()
+		for rk := range info {
+			minfo[rk] = append(minfo[rk], info[rk]...)
+		}
+
+		v.ClearReportInfo()
+	}
+	report(minfo)
+}
+
+func reportByArr(arr []*gobot.Bot) {
+	minfo := make(map[string][]botreport.Info)
+	for _, v := range arr {
+		info := v.GetReportInfo()
+		for rk := range info {
+			minfo[rk] = append(minfo[rk], info[rk]...)
+		}
+
+		v.ClearReportInfo()
+	}
+	report(minfo)
+}
+
+func staticBot(ports []string, ch chan os.Signal) {
+
+	var bots []*gobot.Bot
+
+	for i := 0; i < num; i++ {
+
+		bot := createBot(ports[rand.Intn(len(ports))])
+		bot.Run()
+
+		bots = append(bots, bot)
+	}
+
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		for {
+			select {
+			case <-ticker.C:
+				reportByArr(bots)
+			default:
+			}
+		}
+
+	}()
+
+	<-ch
+	fmt.Println("closed")
+	for _, bot := range bots {
+		bot.Close()
+	}
+}
+
+func createBot(port string) *gobot.Bot {
+	md := &bbprefab.BotData{}
+	bot := gobot.New(gobot.BotConfig{
+		Addr:   "http://localhost:" + port,
+		Report: false,
+	}, md)
+
+	bot.Timeline.AddStep(bbsteps.NewGuestLoginStep(md))
+	bot.Timeline.AddStep(bbsteps.NewLoginOutStep(md))
+	bot.Timeline.AddLoopStep(bbsteps.NewRenameStep(md))
+
+	return bot
+}
+
+func increaseBot(ports []string, ch chan os.Signal) {
+
+	botm := make(map[string]*gobot.Bot)
+	ticker := time.NewTicker(time.Second)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+
+				for i := 0; i < num; i++ {
+					bot := createBot(ports[rand.Intn(len(ports))])
+					bot.Run()
+					time.AfterFunc(time.Duration(lifetime)*time.Second, func() {
+						bot.Close()
+
+						reportByMap(botm)
+
+						delete(botm, bot.ID())
+					})
+
+					botm[bot.ID()] = bot
+				}
+			default:
+			}
+		}
+	}()
+
+	<-ch
+	fmt.Println("closed")
+}
 
 func main() {
 
@@ -60,68 +181,23 @@ func main() {
 		return
 	}
 
-	tokenm = make(map[string]int)
+	fmt.Println("===== run bot =====")
+	fmt.Println("target", target)
+	fmt.Println("num", num)
+	fmt.Println("increase", increase)
+	fmt.Println("lifetime", lifetime)
 
 	rand.Seed(time.Now().UnixNano())
 	ports := []string{"14001", "14002" /*, "14003"*/}
 
-	var bots []*gobot.Bot
-
-	for i := 0; i < num; i++ {
-
-		md := &bbprefab.BotData{}
-		bot := gobot.New(gobot.BotConfig{
-			Addr:   "http://localhost:" + ports[rand.Intn(len(ports))],
-			Report: false,
-		}, md)
-
-		bot.Timeline.AddStep(bbsteps.NewGuestLoginStep(md))
-		bot.Timeline.AddStep(bbsteps.NewLoginOutStep(md))
-		bot.Timeline.AddLoopStep(bbsteps.NewRenameStep(md))
-
-		bot.Run()
-
-		bots = append(bots, bot)
-	}
-
-	go func() {
-		ticker := time.NewTicker(time.Second)
-		for {
-			select {
-			case <-ticker.C:
-				minfo := make(map[string][]botreport.Info)
-				for _, v := range bots {
-					r := v.GetReport()
-					for rk := range r.Info {
-						minfo[rk] = append(minfo[rk], r.Info[rk]...)
-					}
-
-					r.Clear()
-				}
-
-				for k := range minfo {
-					t, err := botreport.GetAverageTime(minfo[k])
-					if err != nil {
-						continue
-					}
-
-					rate := botreport.GetSuccRate(minfo[k])
-
-					fmt.Printf("%-30s Req count %-5d Average time %-5s Succ rate %-10s \n", k, len(minfo[k]), strconv.Itoa(t)+"ms", rate)
-				}
-
-			default:
-			}
-		}
-
-	}()
-
 	ch := make(chan os.Signal)
 	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT)
-	<-ch
 
-	for _, v := range bots {
-		v.Close()
+	if increase {
+		increaseBot(ports, ch)
+	} else {
+		staticBot(ports, ch)
 	}
+
 	time.Sleep(time.Second)
 }
